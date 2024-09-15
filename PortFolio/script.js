5c45412c80d446efbda6fc75683618a92a7b8fc9
CharanChery <charanchery70@gmail.com>
2024-09-15 20:44:27 +0530

    Update script.js

--- PortFolio/script.js
+++ PortFolio/script.js
@@ -3,3 +3,12 @@ if (window.innerWidth <= 768) {
     "This website is best viewed on a desktop. Please use a laptop or desktop for a better experience."
   );
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector(".menu-icon");
+  const navList = document.querySelector(".navlist");
+
+  menuIcon.addEventListener("click", function () {
+    navList.classList.toggle("open");
+  });
+});
