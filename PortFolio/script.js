65b931aea621c6eb58b9e4b7ebf0d3cc834ed459
CharanChery <charanchery70@gmail.com>
2024-09-29 23:27:28 +0530

    nav bar caution

--- PortFolio/script.js
+++ PortFolio/script.js
@@ -12,3 +12,9 @@ document.addEventListener("DOMContentLoaded", function () {
     navList.classList.toggle("open");
   });
 });
+document.getElementById("About_me").addEventListener("click", function () {
+  alert("On developing mode, please be patience on navigation bar");
+});
+document.getElementById("Education_bgv").addEventListener("click", function () {
+  alert("On developing mode, please be patience on navigation bar");
+});
